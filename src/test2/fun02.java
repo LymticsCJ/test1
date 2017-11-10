6a1f9dfeacd67e1eba88d2b6ee5b7e5a3bc86d0f
LymticsCJ <769558555@qq.com>
2017-11-10 10:56:08 +0800

    day09

---
+++ src/test2/fun02.java
@@ -0,0 +1,13 @@
+package test2;  
+/**  
+ * ClassName:fun02 <br/>  
+ * Function:  <br/>  
+ * Date:     Nov 10, 2017 10:54:52 AM <br/>       
+ */
+public class fun02 {
+    
+    public static void main(String[] args) {
+        System.out.println("hello world");
+    }
+}
+  
